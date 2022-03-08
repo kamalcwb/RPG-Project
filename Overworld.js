c5175e7c76799480e9878c4052e9552e32a03bd5
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-03-07 21:43:37 -0300

    update
    add some hero config.
    add images

--- Overworld.js
+++ Overworld.js
@@ -5,6 +5,29 @@ class Overworld {
         this.ctx = this.canvas.getContext("2d")
     }
     init() {
-        console.log("Olá Mundo", this);
+        const image = new Image();
+        image.onload = () => {
+            this.ctx.drawImage(image)
+        };
+        image.src = "/images/maps/DemoLower.png"
+
+        const x = 0
+        const y = 0
+        const hero = new Image()
+        hero.onload = () => {
+            this.ctx.drawImage(
+                hero,
+                0, //left cut
+                0,  //top cut
+                32, //whidth cut
+                32, //hight cut
+                x,
+                y,
+                32,
+                32
+            )
+
+        }
+        hero.src = "./images/characters/people/hero.png"
     }
 }
