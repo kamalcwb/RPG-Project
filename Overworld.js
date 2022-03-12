d333e009beda1bbe6c7f3a01b746365e0a6d1ac4
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-03-12 18:21:57 -0300

    Paredes, Falas
    Configurando locais que não são possíveis andar
    Adicionado sistema de fala com NPCs

--- Overworld.js
+++ Overworld.js
@@ -1,10 +1,10 @@
 class Overworld {
-  constructor(config) {
-    this.element = config.element;
-    this.canvas = this.element.querySelector(".game-canvas");
-    this.ctx = this.canvas.getContext("2d");
-    this.map = null;
-  }
+ constructor(config) {
+   this.element = config.element;
+   this.canvas = this.element.querySelector(".game-canvas");
+   this.ctx = this.canvas.getContext("2d");
+   this.map = null;
+ }
 
   startGameLoop() {
     const step = () => {
@@ -17,7 +17,8 @@ class Overworld {
       //Update all objects
       Object.values(this.map.gameObjects).forEach(object => {
         object.update({
-          arrow: this.directionInput.direction
+          arrow: this.directionInput.direction,
+          map: this.map,
         })
       })
 
@@ -25,26 +26,68 @@ class Overworld {
       this.map.drawLowerImage(this.ctx, cameraPerson);
 
       //Draw Game Objects
-      Object.values(this.map.gameObjects).forEach(object => {
+      Object.values(this.map.gameObjects).sort((a,b) => {
+        return a.y - b.y;
+      }).forEach(object => {
         object.sprite.draw(this.ctx, cameraPerson);
       })
 
       //Draw Upper layer
       this.map.drawUpperImage(this.ctx, cameraPerson);
-
+      
       requestAnimationFrame(() => {
-        step();
+        step();   
       })
     }
     step();
-  }
+ }
 
-  init() {
-    this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+ bindActionInput() {
+   new KeyPressListener("Enter", () => {
+     //Is there a person here to talk to?
+     this.map.checkForActionCutscene()
+   })
+ }
 
-    this.directionInput = new DirectionInput();
-    this.directionInput.init();
+ bindHeroPositionCheck() {
+   document.addEventListener("PersonWalkingComplete", e => {
+     if (e.detail.whoId === "hero") {
+       //Hero's position has changed
+       this.map.checkForFootstepCutscene()
+     }
+   })
+ }
 
-    this.startGameLoop();
-  }
+ startMap(mapConfig) {
+  this.map = new OverworldMap(mapConfig);
+  this.map.overworld = this;
+  this.map.mountObjects();
+ }
+
+ init() {
+  this.startMap(window.OverworldMaps.DemoRoom);
+
+
+  this.bindActionInput();
+  this.bindHeroPositionCheck();
+
+  this.directionInput = new DirectionInput();
+  this.directionInput.init();
+
+  this.startGameLoop();
+
+
+  // this.map.startCutscene([
+  //   { who: "hero", type: "walk",  direction: "down" },
+  //   { who: "hero", type: "walk",  direction: "down" },
+  //   { who: "npcA", type: "walk",  direction: "up" },
+  //   { who: "npcA", type: "walk",  direction: "left" },
+  //   { who: "hero", type: "stand",  direction: "right", time: 200 },
+  //   { type: "textMessage", text: "WHY HELLO THERE!"}
+  //   // { who: "npcA", type: "walk",  direction: "left" },
+  //   // { who: "npcA", type: "walk",  direction: "left" },
+  //   // { who: "npcA", type: "stand",  direction: "up", time: 800 },
+  // ])
+
+ }
 }
--- sprite.js
+++ sprite.js
@@ -20,21 +20,21 @@ class Sprite {
 
     //Configure Animation & Initial State
     this.animations = config.animations || {
-      "idle-down": [[0, 0]],
-      "idle-right": [[0, 1]],
-      "idle-up": [[0, 2]],
-      "idle-left": [[0, 3]],
-      "walk-down": [[1, 0], [0, 0], [3, 0], [0, 0],],
-      "walk-right": [[1, 1], [0, 1], [3, 1], [0, 1],],
-      "walk-up": [[1, 2], [0, 2], [3, 2], [0, 2],],
-      "walk-left": [[1, 3], [0, 3], [3, 3], [0, 3],]
+      "idle-down" : [ [0,0] ],
+      "idle-right": [ [0,1] ],
+      "idle-up"   : [ [0,2] ],
+      "idle-left" : [ [0,3] ],
+      "walk-down" : [ [1,0],[0,0],[3,0],[0,0], ],
+      "walk-right": [ [1,1],[0,1],[3,1],[0,1], ],
+      "walk-up"   : [ [1,2],[0,2],[3,2],[0,2], ],
+      "walk-left" : [ [1,3],[0,3],[3,3],[0,3], ]
     }
     this.currentAnimation = "idle-right"; // config.currentAnimation || "idle-down";
     this.currentAnimationFrame = 0;
 
     this.animationFrameLimit = config.animationFrameLimit || 8;
     this.animationFrameProgress = this.animationFrameLimit;
-
+    
 
     //Reference the game object
     this.gameObject = config.gameObject;
@@ -69,7 +69,7 @@ class Sprite {
 
 
   }
-
+  
 
   draw(ctx, cameraPerson) {
     const x = this.gameObject.x - 8 + utils.withGrid(10.5) - cameraPerson.x;
@@ -82,9 +82,9 @@ class Sprite {
 
     this.isLoaded && ctx.drawImage(this.image,
       frameX * 32, frameY * 32,
-      32, 32,
-      x, y,
-      32, 32
+      32,32,
+      x,y,
+      32,32
     )
 
     this.updateAnimationProgress();
